697ee905d86c2dae5b79aaa053a3e4f857f0e1a4
CheveuCodeur <cheveucodeur@grominet.home>
2022-02-02 07:23:35 +0100

    Modif ReverseHelper

--- src/Service/ReverseHelper.php
+++ src/Service/ReverseHelper.php
@@ -7,11 +7,8 @@ class ReverseHelper
     public function reverse(string $string): string
     {
         $result = array_reverse(explode(' ', $string));
-        $result[0]=ucfirst($result[0]);
-        $stringReversed = '';
-        foreach ($result as $value) {
-            $stringReversed .= ' ' . $value;
-        }
+        $result[0] = ucfirst($result[0]);
+        $stringReversed = \implode(' ', $result);
 
         return $stringReversed;
     }
